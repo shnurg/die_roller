0e08261d9e4dc42e272e12d155155dbca4e72a1a
Andrew Thomas <thomasaj@uchicago.edu>
2014-01-22 14:00:42 -0600

    supported negative modifiers
    modified read_roll and roll_value to support negative modifiers

--- roller.c
+++ roller.c
@@ -4,33 +4,31 @@
 #include <time.h>
 #include <ctype.h>
 
-void read_roll(char* str, int* d, int* s, int* m);
-int roll(int d, int s, int m);
+char* error = "Malformed roll argument: must have form '[num_dice]d[num_sides][+/-][modifier]'\n";
 
-int main(int argc, char* argv[])
-{
-    if(argc < 2){
-        printf("Malformed roll argument: must have form '[num_dice]d[num_sides]+[modifier]'\n");
-    } else {
-        int d=0, s=0, m=0;
-        read_roll(argv[1],&d,&s,&m);
-        printf("Total roll: %d\n",roll(d,s,m));
-    }
-    return EXIT_SUCCESS;
-}
-
-
-// note: NEED TO FIX THIS TO ACCOMODATE NEGATIVE MODIFIERS
 void read_roll(char* str, int* d, int* s, int* m)
 {
     // this part checks for the case in which arg is formed like 'd+3'.
     int i=0;
     while(*(str+i) != 'd') i++; // find the d
     if(!isdigit(*(str+i+1))){   // if not immediately followed by a digit, then die
-        printf("Malformed roll argument: must have form '[num_dice]d[num_sides]+[modifier]'\n");
+        printf("%s",error);
         exit(1);
     }
-    
+
+    // 1 if minus sign found, -1 if plus sign found, 0 otherwise
+    int sign = 0; 
+    char* it = str;
+    while(*it++ != '\0'){
+        if(*it == '+'){
+            sign++;
+            break;
+        } else if(*it == '-'){
+            sign--;
+            break;
+        }
+    }
+
     /*  How sscanf can fail here :
         - arg is of form 2d+3
             d assigned, s not found so returns 1. never valid.
@@ -39,37 +37,63 @@ void read_roll(char* str, int* d, int* s, int* m)
         - arg is of form 2d6
             returns 2. and should be a valid thing.
     */
-    
-    int c = sscanf(str,"%dd%d+%d",d,s,m);
-    
-    if(c<2){
-        if(c==1){
-            printf("Malformed roll argument: must have form '[num_dice]d[num_sides]+[modifier]'\n");
-            exit(1);
-        } else if(c==0){
-            if((c=sscanf(str,"d%d+%d",s,m)) == 0){
-                printf("Malformed roll argument: must have form '[num_dice]d[num_sides]+[modifier]'\n");
+    if(sign == 1){
+        int c = sscanf(str,"%dd%d+%d",d,s,m);
+        if(c<2){
+            if(c==1){
+                printf("%s",error);
                 exit(1);
+            } else if(c==0){
+                if((c=sscanf(str,"d%d+%d",s,m)) == 0){
+                    printf("%s",error);
+                    exit(1);
+                }
             }
         }
-    }
-    
-    if(c == 3){                                 // 2d20+3
-        printf("Rolling %dd%d+%d.\n",*d,*s,*m);
-    } else if(c == 2 && *d == 0){               // d20+3
-        printf("Rolling d%d+%d.\n",*s,*m);
-    } else if(c == 2 && *m == 0){               // 2d20
-        printf("Rolling %dd%d.\n",*d,*s);
-    } else if(c == 1 && *d == 0 && *m == 0){    // d20
-        printf("Rolling d%d.\n",*s);
+        if(c == 3){                                 // 2d20+3
+            printf("Rolling %dd%d+%d.\n",*d,*s,*m);
+        } else if(c == 2 && *d == 0){               // d20+3
+            printf("Rolling d%d+%d.\n",*s,*m);
+        } else if(c == 2 && *m == 0){               // 2d20
+            printf("Rolling %dd%d.\n",*d,*s);
+        } else if(c == 1 && *d == 0 && *m == 0){    // d20
+            printf("Rolling d%d.\n",*s);
+        } else {
+            printf("%s",error);
+            exit(1);
+        }
+    } else if(sign == -1) { // repeated since not easy to change how string is parsed
+        int c = sscanf(str,"%dd%d-%d",d,s,m);
+        if(c<2){
+            if(c==1){
+                printf("%s",error);
+                exit(1);
+            } else if(c==0){
+                if((c=sscanf(str,"d%d-%d",s,m)) == 0){
+                    printf("%s",error);
+                    exit(1);
+                }
+            }
+        }
+        if(c == 3){                                 // 2d20+3
+            printf("Rolling %dd%d-%d.\n",*d,*s,*m);
+        } else if(c == 2 && *d == 0){               // d20+3
+            printf("Rolling d%d-%d.\n",*s,*m);
+        } else if(c == 2 && *m == 0){               // 2d20
+            printf("Rolling %dd%d.\n",*d,*s);
+        } else if(c == 1 && *d == 0 && *m == 0){    // d20
+            printf("Rolling d%d.\n",*s);
+        } else {
+            printf("%s",error);
+            exit(1);
+        }
+        *m = -*m;
     } else {
-        printf("Malformed roll argument: must have form '[num_dice]d[num_sides]+[modifier]'\n");  
-        exit(1);
+        printf("%s",error);
     }
-
 }
 
-int roll(int d, int s, int m)
+int roll_value(int d, int s, int m)
 {
     int i, new, result=0;
     srand(time(NULL));
@@ -81,7 +105,19 @@ int roll(int d, int s, int m)
         result+=new;
     }
     
-    if(m>0) result+=m;
+    result+=m;
     
     return result;
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc < 2){
+        printf("%s",error);
+    } else {
+        int d=0, s=0, m=0;
+        read_roll(argv[1],&d,&s,&m);
+        printf("Total roll: %d\n",roll_value(d,s,m));
+    }
+    return EXIT_SUCCESS;
 }
